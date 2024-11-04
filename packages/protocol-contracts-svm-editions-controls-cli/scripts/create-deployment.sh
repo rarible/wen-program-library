6e0a36d1b94dc613707814a761fb1b060d6e53e9
Vadim Fadeev <xboxfadeev@gmail.com>
2024-11-04 15:14:35 +0000

    feat: add bash scripts

--- packages/protocol-contracts-svm-editions-controls-cli/scripts/create-deployment.sh
+++ packages/protocol-contracts-svm-editions-controls-cli/scripts/create-deployment.sh
@@ -2,7 +2,7 @@ npx ts-node ./src/createDeployment.ts \
   -t BTicWgGhoTsBANuirXS7UCeF6bxfvTnCaxM5HQRRfGZS \
   -k ~/.config/solana/id.json \
   -r https://api.devnet.solana.com \
-  -s CAT2 \
+  -s CAT3 \
   --maxNumberOfTokens 1000 \
   --maxMintsPerWallet 100 \
   -u "ipfs://QmfJh4B8KySR1KHaXRNWkcDBn67ZuJkzAyaVCWAS8Kcezc/0" \
@@ -15,3 +15,8 @@ npx ts-node ./src/createDeployment.ts \
   --extraMeta "type:handmade" "author:Vadim" "value:important" \
   --itemBaseUri "ipfs://QmdHaufjUDJgbZzZ4eFCjtJQyeQpuNwoEvqLm5rq159vC8/{}" \
   --itemBaseName "Cat #{}" 
+
+
+npx ts-node ./src/addPhase.ts -d 5wdChPiHVevvVu3Nnonzsgc92r7ZBJgNbQKC3TRDU7Ct -k ~/.config/solana/id.json -r https://api.devnet.solana.com --maxMintsPerWallet 100 --maxMintsTotal 1000 --priceAmount 500 -s 1709564319 -e 1959564319
+
+npx ts-node ./src/mintWithControls.ts -d 5wdChPiHVevvVu3Nnonzsgc92r7ZBJgNbQKC3TRDU7Ct -k ~/.config/solana/id.json -r https://api.devnet.solana.com -p 0 -n 1
